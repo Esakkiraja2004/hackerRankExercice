dd0e20816fec33aa85eefd2a643d11d06f55bf6d
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-08-07 19:21:34 +0530

    FM

---
+++ Strings/minionGame.py
@@ -0,0 +1,16 @@
+w = 'BANANA'
+s = 1
+k = 1
+v =['A','E','I','O','U','Y']
+
+for i in range(len(w)):
+    if w[i] in v:
+        k += len(w) -i
+    else:
+        s += len(w) -i
+
+if s> k:
+    print('Stuart',s)
+else:
+    print('Kevin', k)
+        
