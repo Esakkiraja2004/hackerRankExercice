0e4b47660f3316a551210980773df2389f8ec59f
Esakkiraja2004 <esakkiraja2401@gmail.com>
2024-08-07 19:25:20 +0530

    FM

--- Strings/minionGame.py
+++ Strings/minionGame.py
@@ -5,9 +5,9 @@ v =['A','E','I','O','U','Y']
 
 for i in range(len(w)):
     if w[i] in v:
-        k = len(w) -i
+        k += len(w) -i
     else:
-        s = len(w) -i
+        s += len(w) -i
 
 if s> k:
     print('Stuart',s)
